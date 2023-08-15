03a6a8272320b13880bd901fab7a270c1ff9f4c1
Ben Glazer <ben@benglazer.com>
2023-08-14 23:06:45 -0500

    Multiple setup.py improvements

--- setup.py
+++ setup.py
@@ -3,20 +3,32 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name='plat',
-    version='0.0.1',
+    version='0.1',
     description='At command interface',
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ben Glazer',
     author_email='ben@benglazer.com',
     url='https://github.com/benglazer/plat',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    license='MIT',
+    license_files=['LICENSE'],
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Shells',
+        'Topic :: System :: Systems Administration',
+    ],
 )
